bf8298364d87aff3099151690a12d64e893a4a21
John Papa <john@Johns-MBP.lan>
2018-01-29 12:52:37 -0500

    initial routing and heroes layout with angular material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'heroes', loadChildren: 'app/heroes/heroes.module#HeroesModule' },
+  { path: '**', pathMatch: 'full', component: PathNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
