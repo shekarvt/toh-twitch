a32e04f89d7b6bb7b2f8a0cdaedbb61369eabf66
John Papa <john@Johns-MBP.lan>
2018-02-07 14:47:50 -0500

    fix the routing, cuz julie and imaple said to

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,12 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'heroes' },
-  { path: 'heroes', component: HeroesComponent },
+  { path: '', pathMatch: 'full', component: HeroesComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class HeroesRoutingModule { }
+export class HeroesRoutingModule {}
